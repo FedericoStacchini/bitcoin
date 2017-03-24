12ce6c2bc4e820273e7f0bd45bdfa23eb56d5813
FedericoStacchini <federico.stacchini69@gmail.com>
2017-03-24 23:42:32 +0100

    Step 2 Network (libeay32.dll e ssleay32.dll)

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,16 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QNetworkAccessManager>
+#include <QNetworkRequest>
+#include <QNetworkReply>
+#include <QUrl>
+#include <QUrlQuery>
+#include <QVariant>
+#include <QJsonValue>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QVariantMap>
+#include <QJsonArray>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -12,16 +23,55 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+QJsonObject sendRequest() {
+
+    // create custom temporary event loop on stack
+    QEventLoop eventLoop;
+
+    // "quit()" the event-loop, when the network request "finished()"
+    QNetworkAccessManager mgr;
+    QObject::connect(&mgr, SIGNAL(finished(QNetworkReply*)), &eventLoop, SLOT(quit()));
+
+    // the HTTP request
+    QNetworkRequest req( QUrl( QString("https://www.bitstamp.net/api/v2/ticker/btceur/") ) );
+    QNetworkReply *reply = mgr.get(req);
+    eventLoop.exec(); // blocks stack until "finished()" has been called
+
+    if (reply->error() == QNetworkReply::NoError) {
+
+        QString strReply = (QString)reply->readAll();
+
+        //parse json
+        //qDebug() << "Response:" << strReply;
+        QJsonDocument jsonResponse = QJsonDocument::fromJson(strReply.toUtf8());
+
+        QJsonObject jsonObj = jsonResponse.object();
+
+        delete reply;
+        return jsonObj;
+    }
+    else {
+        //failure
+        qDebug() << "Failure" <<reply->errorString();
+        delete reply;
+        return sendRequest();
+    }
+}
 
 void MainWindow::on_pushButton_clicked()
 {
-ui->label->setText("Time:");
-ui->label_2->setText("Volume:");
-ui->label_3->setText("High:");
-ui->label_4->setText("Low:");
-ui->label_5->setText("Open:");
-ui->label_6->setText("Last:");
-ui->label_7->setText("Ask:");
-ui->label_8->setText("Bid:");
-ui->label_9->setText("Vwap:");
+    QJsonObject jsonObj = sendRequest();
+    ui->label->setText("Time:" + QDateTime::fromTime_t(jsonObj["timestamp"].toString().toULongLong()).toString("hh:mm:ss"));
+    ui->label_2->setText("Volume:" + jsonObj["volume"].toString());
+    ui->label_3->setText("High:" + jsonObj["high"].toString());
+    ui->label_4->setText("Low:" + jsonObj["low"].toString());
+    ui->label_5->setText("Open:" + jsonObj["open"].toString());
+    ui->label_6->setText("Last:" + jsonObj["last"].toString());
+    ui->label_7->setText("Ask:" + jsonObj["ask"].toString());
+    ui->label_8->setText("Bid:"+ jsonObj["bid"].toString());
+    ui->label_9->setText("Vwap:" + jsonObj["vwap"].toString());
+
+    ui->statusBar->showMessage(QDate::currentDate().toString("dd/MM/yyyy") + " " + QTime::currentTime().toString("hh:mm:ss"));
 }
+
+
